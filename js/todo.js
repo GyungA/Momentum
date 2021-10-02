63d5feda9e74bc2117c3f4311742efea2a5e2c31
GyungA <gga612@naver.com>
2021-10-02 13:34:18 +0900

    add: loading ToDos

--- js/todo.js
+++ js/todo.js
@@ -1,6 +1,12 @@
 const toDoForm = document.querySelector("#todo-form");
 const toDoInput = toDoForm.querySelector("input");
 const toDoList = document.querySelector("#todo-list");
+let toDos = [];
+const TODOS_KEYS = "todos";
+
+function saveToDos() {
+  localStorage.setItem(TODOS_KEYS, JSON.stringify(toDos));
+}
 
 function paintToDo(newTodo){
     const item = document.createElement("li");
@@ -22,7 +28,16 @@ function handleToDoSubmit(event) {
   event.preventDefault();
   const newTodo = toDoInput.value;
   toDoInput.value = "";
+  toDos.push(newTodo);
   paintToDo(newTodo);
+  saveToDos();
 }
 
 toDoForm.addEventListener("submit", handleToDoSubmit);
+const savedToDos = localStorage.getItem(TODOS_KEYS);
+
+if(savedToDos !== null){
+  const parsedToDos=JSON.parse(savedToDos);
+  toDos = parsedToDos;
+  parsedToDos.forEach(paintToDo);
+}
